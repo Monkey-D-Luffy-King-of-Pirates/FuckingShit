00ae2c1e29ef124bdcaf07c80f9605e406e0a911
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-07-02 18:31:21 +0400

    обязательно

--- FuckingShit/Program.cs
+++ FuckingShit/Program.cs
@@ -40,15 +40,16 @@ namespace FuckingShit
           Pizzaz november = new Pizzaz() { Zippo = 3 };
           Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
           while (tango.Lala(foxtrot.Zippo))
-            {
+          {
                 november.Zippo *= -1;
                 november.Bamboo(tango.Vavavoom);
                 foxtrot.Bamboo(november.Zippo);
                 tango.Vavavoom -= foxtrot.Zippo;
-            }
+          }
             Console.WriteLine("november.Zippo = " + november.Zippo);
             Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
             Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+            Console.ReadKey();
         }
     }
 }
